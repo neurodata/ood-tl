1bb4e7f677c0a66a4cf38d517c8376360b8791b0
Laknath1996 <ashwindesilva@yahoo.com>
2022-07-06 16:19:01 -0400

    Update

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
-python singlehead_dual_tasks.py --in_task 2 --out_task 1 3 4 5
-python singlehead_dual_tasks.py --in_task 1 --out_task 2 3 4 5
-python singlehead_dual_tasks.py --in_task 3 --out_task 1 2 4 5
-python singlehead_dual_tasks.py --in_task 4 --out_task 1 2 3 5
-python singlehead_dual_tasks.py --in_task 5 --out_task 1 2 3 4
+python singlehead_dual_tasks.py --in_task 2 --out_task 3
+# python singlehead_dual_tasks.py --in_task 2 --out_task 1 3 4 5
+# python singlehead_dual_tasks.py --in_task 1 --out_task 2 3 4 5
+# python singlehead_dual_tasks.py --in_task 3 --out_task 1 2 4 5
+# python singlehead_dual_tasks.py --in_task 4 --out_task 1 2 3 5
+# python singlehead_dual_tasks.py --in_task 5 --out_task 1 2 3 4
