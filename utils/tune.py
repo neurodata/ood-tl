59a73e081361a0041cde432e65f8351b99464ff9
Laknath1996 <ashwindesilva@yahoo.com>
2022-07-07 03:37:59 +0000

    Update

--- utils/tune.py
+++ utils/tune.py
@@ -5,7 +5,7 @@ from copy import deepcopy
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
 
-def train(net, hp, train_loader, optimizer, lr_scheduler, gpu, task_id_flag=False, verbose=False, alpha=None, patience=3):
+def train(net, hp, train_loader, optimizer, lr_scheduler, gpu, task_id_flag=False, verbose=False, alpha=None, patience=5):
   device = torch.device(gpu if torch.cuda.is_available() else 'cpu')
   net.to(device)
 
@@ -21,7 +21,6 @@ def train(net, hp, train_loader, optimizer, lr_scheduler, gpu, task_id_flag=Fals
 
     for dat, target in train_loader:
         optimizer.zero_grad()
-
         tasks, labels = target
         tasks = tasks.long()
         labels = labels.long()
@@ -119,14 +118,14 @@ def search_alpha(net, dataset, n, hp, gpu, val_split=0.1, SEED=1996):
     tune_trainset = deepcopy(dataset.comb_trainset)
     tune_valset = deepcopy(dataset.comb_trainset)
     tune_trainset.data = np.concatenate((target_X_train, X[n:]))
-    tune_trainset.targets = np.concatenate((target_y_train, y[n:]))
+    tune_trainset.targets = np.concatenate((target_y_train, y[n:])).tolist()
     tune_valset.data = target_X_val
     tune_valset.target = target_y_val
 
     tune_train_loader = DataLoader(tune_trainset, batch_size=hp['batch_size'], shuffle=True, worker_init_fn=wif, pin_memory=True, num_workers=4)
     tune_val_loader = DataLoader(tune_valset, batch_size=len(target_y_val), shuffle=True, worker_init_fn=wif, pin_memory=True, num_workers=4)    
 
-    alpha_range = np.arange(0.5, 1.1, 0.1)
+    alpha_range = np.arange(0.5, 1.05, 0.1)
     scores = []
 
     for alpha in alpha_range:
@@ -142,7 +141,7 @@ def search_alpha(net, dataset, n, hp, gpu, val_split=0.1, SEED=1996):
         print(acc)
         scores.append(acc)
 
-    return alpha_range[np.argmin[score]]
+    return alpha_range[np.argmin(scores)]
 
 
     
