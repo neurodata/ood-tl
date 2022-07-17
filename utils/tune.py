bc9c4076a177eec5d6646175dcbbfe76a5f9146f
Laknath1996 <ashwindesilva@yahoo.com>
2022-07-17 10:34:05 -0400

    Update

--- utils/tune.py
+++ utils/tune.py
@@ -141,7 +141,9 @@ def search_alpha(net, dataset, n, beta, hp, gpu, sensitivity=0.05, val_split=0.1
     test_loader = dataset.get_task_data_loader(0, 100, train=False)
 
     # alpha_range = np.arange(0.5, 1+1e-5, sensitivity)
-    alpha_range = np.arange(0.9, 1+1e-5, 0.01)
+    # alpha_range = np.arange(0.9, 1+1e-5, 0.01)
+    alpha_range = np.concatenate((np.arange(0.5, 0.9, 0.05), np.arange(0.9, 1+1e-4, 0.005)))
+
     scores = []
  
     for alpha in alpha_range:
