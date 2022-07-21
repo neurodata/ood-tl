5d0bc365111e7c949a31f27234702f180012b3d5
Laknath1996 <ashwindesilva@yahoo.com>
2022-07-20 23:21:47 -0400

    Update

--- singlehead_rotated_tasks.py
+++ singlehead_rotated_tasks.py
@@ -27,16 +27,6 @@ def run_experiment(exp_conf, gpu):
     hp = exp_conf['hp']
     df = pd.DataFrame()
 
-    if exp_conf['task_aware']:
-        if exp_conf['tune_alpha']:
-            setting = "Optimal_Task_Aware"
-        else:
-            setting = "Naive_Task_Aware"
-    else:
-        setting = "Task_Agnostic"
-
-    print("Experimental Setting : ", setting)
-
     for angle in exp_conf['angles']:
         print("Doing angle = {}".format(angle))
         dataset = RotatedCIFAR10Handler(exp_conf['task'], angle)
@@ -94,10 +84,10 @@ def run_experiment(exp_conf, gpu):
         
         print("Saving individual results...")
         dfi = df.filter(['m', 'r', str(angle)])
-        dfi.to_csv('{}/{}_{}_{}_{}_{}.csv'.format(exp_conf['save_folder'], exp_conf['dataset'], exp_conf['net'], exp_conf['exp_name'], angle, setting))
+        dfi.to_csv('{}/{}_{}_{}_{}.csv'.format(exp_conf['save_folder'], exp_conf['dataset'], exp_conf['net'], exp_conf['exp_name'], angle))
     
     print("Saving bulk results...")
-    df.to_csv('{}/{}_{}_{}_{}.csv'.format(exp_conf['save_folder'], exp_conf['dataset'], exp_conf['net'], exp_conf['exp_name'], setting))
+    df.to_csv('{}/{}_{}_{}.csv'.format(exp_conf['save_folder'], exp_conf['dataset'], exp_conf['net'], exp_conf['exp_name']))
 
 def main():
     parser = argparse.ArgumentParser()
@@ -127,6 +117,18 @@ def main():
     exp_folder_path = os.path.join(exp_conf['save_folder'], str(datetime.datetime.now()))
     os.makedirs(exp_folder_path)
     exp_conf['save_folder'] = exp_folder_path
+
+    if exp_conf['task_aware']:
+        if exp_conf['tune_alpha']:
+            setting = "Optimal_Task_Aware"
+        else:
+            setting = "Naive_Task_Aware"
+    else:
+        setting = "Task_Agnostic"
+
+    print("Experimental Setting : ", setting)
+    exp_conf['setting'] = setting
+
     with open('exp_config.yml', 'w') as outfile:
         yaml.dump(exp_conf, outfile, default_flow_style=False)
 
