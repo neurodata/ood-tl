452627366d0cdf3ac1073afed5411bb6ee3eb663
Laknath1996 <ashwindesilva@yahoo.com>
2022-07-24 01:25:32 -0400

    Update

--- run_gpu_0.sh
+++ run_gpu_0.sh
@@ -4,14 +4,14 @@ python singlehead_rotated_tasks.py --angles 0 10 45 90 135 --no-task_aware --no-
 # CIFAR-10 T2 vs theta-T2 + SmallConv + Aug + Target-Fraction Preserving Batches (Naive Task Aware)
 python singlehead_rotated_tasks.py --angles 0 10 45 90 135 --task_aware --no-tune_alpha --net smallconv --augment --epochs 100 --batch_size 128 --reps 10 --gpu cuda:0 --makefolder
 
-# CIFAR-10 T2 vs theta-T2 + SmallConv + Aug + Target-Fraction Preserving Batches (Optimal Task Agnostic)
-python singlehead_rotated_tasks.py --angles 135 --task_aware --tune_alpha --net smallconv --augment --epochs 100 --batch_size 128 --reps 10 --gpu cuda:0 --makefolder
-
 # CIFAR-10 T2 vs theta-T2 + SmallConv + No Aug + Conventional Batches (Task Agnostic)
 python singlehead_rotated_tasks.py --angles 0 10 45 90 135 --no-task_aware --no-tune_alpha --net smallconv --no-augment --epochs 100 --batch_size 128 --reps 10 --gpu cuda:0 --makefolder
 
 # CIFAR-10 T2 vs theta-T2 + SmallConv + No Aug + Target-Fraction Preserving Batches (Naive Task Aware)
 python singlehead_rotated_tasks.py --angles 0 10 45 90 135 --task_aware --no-tune_alpha --net smallconv --no-augment --epochs 100 --batch_size 128 --reps 10 --gpu cuda:0 --makefolder
+
+# CIFAR-10 T2 vs theta-T2 + SmallConv + Aug + Target-Fraction Preserving Batches (Optimal Task Agnostic)
+python singlehead_rotated_tasks.py --angles 135 --task_aware --tune_alpha --net smallconv --augment --epochs 100 --batch_size 128 --reps 10 --gpu cuda:0 --makefolder
 
 # CIFAR-10 T2 vs theta-T2 + SmallConv + No Aug + Target-Fraction Preserving Batches (Optimal Task Aware)
 python singlehead_rotated_tasks.py --angles 135 --task_aware --tune_alpha --net smallconv --no-augment --epochs 100 --batch_size 128 --reps 10 --gpu cuda:0 --makefolder
