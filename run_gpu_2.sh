350319dda31768901626a9ca0b5799a726c6a95a
Laknath1996 <ashwindesilva@yahoo.com>
2022-07-24 12:58:51 -0400

    Update

--- run_gpu_2.sh
+++ run_gpu_2.sh
@@ -2,4 +2,7 @@
 # python singlehead_dual_tasks.py --in_task 1 --out_task 5 --task_aware --tune_alpha --net smallconv --augment --epochs 100 --batch_size 128 --reps 10 --gpu cuda:2 --makefolder
 
 # (Task Agnostic) T2 vs T3 + SmallConv + Aug + Conventional Batches (bs = 128, e = 100) ()
-python singlehead_dual_tasks.py --exp_id exp --in_task 2 --out_task 3 --no-task_aware --no-tune_alpha --net smallconv --augment --no-custom_sampler --epochs 100 --batch_size 128 --reps 10 --gpu cuda:2 --makefolder
+# python singlehead_dual_tasks.py --exp_id exp --in_task 2 --out_task 3 --no-task_aware --no-tune_alpha --net smallconv --augment --no-custom_sampler --epochs 100 --batch_size 128 --reps 10 --gpu cuda:2 --makefolder
+
+# (Naive Task Aware) T2 vs T3 + SmallConv + Aug + Conventional Batches (bs = 128, e = 100) ()
+python singlehead_dual_tasks.py --exp_id exp --in_task 2 --out_task 3 --task_aware --no-tune_alpha --net smallconv --augment --custom_sampler --epochs 100 --batch_size 128 --reps 10 --gpu cuda:2 --makefolder
