c6bb4581a5070aff7d2d810521419e1794001777
Laknath1996 <ashwindesilva@yahoo.com>
2022-07-24 00:25:41 -0400

    Update

--- run_gpu_3.sh
+++ run_gpu_3.sh
@@ -1,4 +1,4 @@
 git pull
 
 # T1 vs T5 + SmallConv + Aug + Traget-Fraction-Preserving Batches (bs = 128, e = 200)
-python singlehead_dual_tasks.py --in_task 1 --out_task 5 --task_aware --no-tune_alpha --net smallconv --augment --epochs 100 --batch_size 128 --reps 10 --gpu cuda:3
+python singlehead_dual_tasks.py --in_task 1 --out_task 5 --task_aware --no-tune_alpha --net smallconv --augment --epochs 100 --batch_size 128 --reps 10 --gpu cuda:3 --makefolder
