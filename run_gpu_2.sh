698d92c4715ab81684f3dcb77414772ca8efb888
Laknath1996 <ashwindesilva@yahoo.com>
2022-07-24 17:02:26 -0400

    Update

--- run_gpu_2.sh
+++ run_gpu_2.sh
@@ -5,4 +5,7 @@
 # python singlehead_dual_tasks.py --exp_id exp --in_task 2 --out_task 3 --no-task_aware --no-tune_alpha --net smallconv --augment --no-custom_sampler --epochs 100 --batch_size 128 --reps 10 --gpu cuda:2 --makefolder
 
 # (Naive Task Aware) T2 vs T3 + SmallConv + Aug + Conventional Batches (bs = 128, e = 100) ()
-python singlehead_dual_tasks.py --exp_id exp --in_task 2 --out_task 3 --task_aware --no-tune_alpha --net smallconv --augment --custom_sampler --epochs 100 --batch_size 128 --reps 10 --gpu cuda:2 --makefolder
+# python singlehead_dual_tasks.py --exp_id exp --in_task 2 --out_task 3 --task_aware --no-tune_alpha --net smallconv --augment --custom_sampler --epochs 100 --batch_size 128 --reps 10 --gpu cuda:2 --makefolder
+
+# (Optimal Task Aware) T2 vs T3 + SmallConv + Aug + Conventional Batches (bs = 128, e = 100) ()
+python singlehead_dual_tasks.py --exp_id exp --in_task 2 --out_task 3 --task_aware --tune_alpha --net smallconv --augment --custom_sampler --epochs 100 --batch_size 128 --reps 10 --gpu cuda:2 --makefolder
--- run_gpu_3.sh
+++ run_gpu_3.sh
@@ -8,4 +8,7 @@
 # python singlehead_dual_tasks.py --exp_id exp --in_task 2 --out_task 5 --no-task_aware --no-tune_alpha --net smallconv --augment --no-custom_sampler --epochs 100 --batch_size 128 --reps 10 --gpu cuda:3 --makefolder
 
 # (Naive Task Aware) T2 vs T5 + SmallConv + Aug + Conventional Batches (bs = 128, e = 100) ()
-python singlehead_dual_tasks.py --exp_id exp --in_task 2 --out_task 5 --task_aware --no-tune_alpha --net smallconv --augment --custom_sampler --epochs 100 --batch_size 128 --reps 10 --gpu cuda:3 --makefolder
+# python singlehead_dual_tasks.py --exp_id exp --in_task 2 --out_task 5 --task_aware --no-tune_alpha --net smallconv --augment --custom_sampler --epochs 100 --batch_size 128 --reps 10 --gpu cuda:3 --makefolder
+
+# (Optimal Task Aware) T2 vs T5 + SmallConv + Aug + Conventional Batches (bs = 128, e = 100) ()
+python singlehead_dual_tasks.py --exp_id exp --in_task 2 --out_task 5 --task_aware --tune_alpha --net smallconv --augment --custom_sampler --epochs 100 --batch_size 128 --reps 10 --gpu cuda:3 --makefolder
