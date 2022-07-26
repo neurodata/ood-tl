2a96d58ad905a78a3865fbcac395e7c679cf422c
Rahul Ramesh <rahul13ramesh@gmail.com>
2022-07-26 01:07:50 -0400

    deleting files

--- run_gpu_1.sh
+++
@@ -1,2 +0,0 @@
-# T1 vs T5 + SmallConv + Aug + Conventional Batches (bs = 128, e = 100)
-# python singlehead_dual_tasks.py --in_task 1 --out_task 5 --task_aware --tune_alpha --net smallconv --augment --epochs 100 --batch_size 128 --reps 10 --gpu cuda:1 --makefolder
